d0e0e0bd09034520dcbe0bc845e3bc5aa4d14354
elementchen <cweixiong@hotmail.com>
2026-08-03 16:36:33 +0800

    chore: remove deprecated _PCB, Serial_Log, and _Archive folders from repository

--- _Archive/windows_app_csharp/Esp32BtMicConfig/App.xaml.cs
+++
@@ -1,7 +0,0 @@
-using System.Windows;
-
-namespace Esp32BtMicConfig;
-
-public partial class App : Application
-{
-}
--- _Archive/windows_app_csharp/启动配置应用.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ESP32 Bluetooth Microphone Configurator
-echo =======================================
-echo.
-echo Starting application...
-cd /d "%~dp0"
-Esp32BtMicConfig.exe\Esp32BtMicConfig.exe
-pause
